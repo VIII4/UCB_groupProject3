ede65aa05f827354af95048359c2bf8d0740a77c
Donna Johnson <tjndlc@comcast.net>
2020-06-27 11:32:12 -0700

    added comment, password and arrays in issue n user table

--- models/issue.js
+++ models/issue.js
@@ -2,15 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const issueSchema = new Schema({
-  category: { type: String, required: true },
+  createdby: {type: String, required: true },
+  votecount: {type: Number, required: true },
+  votedby:[{username: String}],   ///ask should this be username from user table??
+  status: {type: String, required: true },
+  category: { type: String, required: true }, 
   descr: {type: String, required: true },
   image1: {type: String},
   image2: {type: String},
   image3: {type: String},
   zipcode: {type: Number,  required: true },
-  status: {type: String, required: true },
+  latlng: {lat: Number, lng: Number  },
   date: { type: Date, default: Date.now,  required: true  },
-  addtlcomments: {type: String}
+  addtlcomments: [{username: String, comment: String}]
 });
 
 const Issue = mongoose.model("Issue", issueSchema);
