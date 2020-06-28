3c7274af7d000b954e9c2e57d2846d0acb8fc804
alvaroluna <luna.alvaro3@gmail.com>
2020-06-28 12:35:15 -0700

    Cleaned up some files

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -2,10 +2,12 @@ import React from "react";
 import ContactsCard from "./CardContent/ContactsCard";
 import AboutCard from "./CardContent/AboutCard";
 import VoteList from "./CardContent/VoteList";
+import LoginCard from "./CardContent/LoginCard";
+import RegisterCard from "./CardContent/RegisterCard";
 import './Card.css';
 
-// DIFFERENCE BTWN DISPLAY AND VISIBILITY - visibility still in doc; affecs div; display not
-// CHOOSING ID VS CLASS - single element? - id; affect multiple? - use className
+// visibility:"hidden" hides element but still in DOM
+// display:"none" removes element from DOM
 
 class Card extends React.Component {
     // card closing methods
@@ -39,6 +41,8 @@ class Card extends React.Component {
                     <ContactsCard header="Local Government Contacts" display="none" />
                     <AboutCard header="About" display="none" />
                     <VoteList header="Issues In Your Area" display="none" />
+                    <LoginCard header="Log In To Care'n" display="none" />
+                    <RegisterCard header="Register for Care'n" display="none" />
                 </div>
 
                 {/* add buttons and stuff here */}
