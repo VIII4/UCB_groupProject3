4a927e687a30beb28a8209ecac46fe426457f492
Lay84 <Iyan.laylor@gmail.com>
2020-06-30 10:06:21 -0700

    created desc from component with state hooks

---
+++ client/src/components/DescForm/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+export default function DescForm({}) {
+  const [
+    /* create input value state and function to change state */
+    inputValue,
+    setValue,
+  ] = useState(/* Set Default State of Input Value*/ "");
+
+  return {};
+}
