fa00969d0c076553f78910dc07e2c98030690fc2
alvaroluna <luna.alvaro3@gmail.com>
2020-07-02 11:06:34 -0700

    Trying to changes again

--- client/src/components/Card/CardContent/VoteList.js
+++ client/src/components/Card/CardContent/VoteList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import '../Card.css';
 
+// messed this up
 class VoteList extends React.Component {
     DoSomething = (event, id = 0) => {
         console.log("Event Triggered");
