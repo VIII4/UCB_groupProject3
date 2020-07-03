2f03f454fd5b442b736825d6e8d6198f3fde1d78
Lay84 <Iyan.laylor@gmail.com>
2020-07-02 20:23:21 -0700

    manual refresh logic added to button, also moved button into main component

--- client/src/components/RefreshBtn/RefreshBtn.js
+++ client/src/components/RefreshBtn/RefreshBtn.js
@@ -1,35 +1,15 @@
-import React from "react";
-import './RefreshBtn.css';
+import React, { useState } from "react";
+import "./RefreshBtn.css";
 
-// NOTE
-// events are (jquery?) usually a seperate js file that is called by 
-// html which listens to actions on the client side and reacts...
-// I like this method better...
-
-class RefreshBtn extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { bool: false };
-    }
-
-    DoSomething = () => {
-        console.log("Event Triggered");
-    }
-
-    //                     //
-    // program entry point //
-    //                     //
-    render() {
-        return (
-            // main sidebar div container
-            <div id="refreshBtnContainer">
-                {/* sidebar open button */}
-                <button className="refreshBtn" onClick={this.DoSomething}>
-                    <i className="fa fa-refresh"></i>
-                </button>
-            </div>
-        )
-    }
+export default function RefreshBtn({ onManualRefreshClick }) {
+  const [bool, setBool] = useState(false);
+  return (
+    // main sidebar div container
+    <div id="refreshBtnContainer">
+      {/* sidebar open button */}
+      <button className="refreshBtn" onClick={onManualRefreshClick}>
+        <i className="fa fa-refresh"></i>
+      </button>
+    </div>
+  );
 }
-
-export default RefreshBtn;
