6287937bbd68c0c7ca05cd167cf069026e57f0ce
alvaroluna <luna.alvaro3@gmail.com>
2020-07-02 21:09:36 -0700

    Added classes to voteList

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -10,37 +10,37 @@ import './Card.css';
 // display:"none" removes element from DOM
 
 export default function Card({
-  localIssues,
-  selectedIssue,
-  setSelectedIssue,
-  onVoteClick,
-  visibility,
+    localIssues,
+    selectedIssue,
+    setSelectedIssue,
+    onVoteClick,
+    visibility,
 }) {
-  //                     //
-  // program entry point //
-  //                     //
-  return (
-    // this.props is coming from App.js --> Main.js --> Card.js
-    <div
-      className="cardContainer"
-      id="cardContainer"
-      style={{ visibility: visibility }}
-    >
-      <div id="cardInnards" className="textBlock">
-        <AboutCard header="About" display="none" />
-        <ContactsCard header="Local Government Contacts" display="none" />
-        <VoteList
-    
-          localIssues={localIssues}
-          onVoteClick={onVoteClick}
-          header="Issues In Your Area"
-          display="none"
-        />
-           <LogInCard header="Log In" display="none" />
-           <SignUpCard header="Sign Up" display="none" />
-      </div>
+    //                     //
+    // program entry point //
+    //                     //
+    return (
+        // this.props is coming from App.js --> Main.js --> Card.js
+        <div
+            className="cardContainer"
+            id="cardContainer"
+            style={{ visibility: visibility }}
+        >
+            <div id="cardInnards" className="textBlock">
+                <AboutCard header="About" display="none" />
+                <ContactsCard header="Local Government Contacts" display="none" />
+                <VoteList
 
-      {/* add buttons and stuff here */}
-    </div>
-  );
+                    localIssues={localIssues}
+                    onVoteClick={onVoteClick}
+                    header="Issues In Your Area"
+                    display="none"
+                />
+                <LogInCard header="Log In" display="none" />
+                <SignUpCard header="Sign Up" display="none" />
+            </div>
+
+            {/* add buttons and stuff here */}
+        </div>
+    );
 }
