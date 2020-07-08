0b246c4bc0f75ca82763d7fee43ad1ab2c13c503
alvaroluna <luna.alvaro3@gmail.com>
2020-07-08 10:17:50 -0700

    Started working on IssuesCard...about to download a carousel package for react

--- client/src/components/Card/CardContent/IssueCard.js
+++ client/src/components/Card/CardContent/IssueCard.js
@@ -3,82 +3,80 @@ import "./IssueCard.css";
 import "../Card.css";
 
 class IssueCard extends React.Component {
-  // card methods
-  CloseCardInnards = () => {
-    // collect html collection --> convert to array
-    var cardInnardsList = Array.prototype.slice.call(
-      document.getElementsByClassName("cardInnards")
-    );
+    // card methods
+    CloseCardInnards = () => {
+        // collect html collection --> convert to array
+        var cardInnardsList = Array.prototype.slice.call(
+            document.getElementsByClassName("cardInnards")
+        );
 
-    // hide all card innards with common className
-    cardInnardsList.forEach((element) => {
-      element.style.display = "none";
-    });
-  };
-  CloseCard = () => {
-    // hide card containter
-    document.getElementById("cardContainer").style.visibility = "hidden";
-    this.CloseCardInnards();
-  };
-  OpenCard = (event, type) => {
-    // first close any open cards
-    this.CloseCardInnards();
+        // hide all card innards with common className
+        cardInnardsList.forEach((element) => {
+            element.style.display = "none";
+        });
+    };
+    CloseCard = () => {
+        // hide card containter
+        document.getElementById("cardContainer").style.visibility = "hidden";
+        this.CloseCardInnards();
+    };
+    OpenCard = () => {
+        // first close any open cards
+        this.CloseCardInnards();
 
-    // show card parent
-    document.getElementById("cardContainer").style.visibility = "visible";
+        // show card parent
+        document.getElementById("cardContainer").style.visibility = "visible";
 
-    // using type input render the appropriate card innards
-    switch (type) {
-      // set to visible by id since they are unique
-      case "about":
-        document.getElementById("aboutCardContent").style.display = "block";
-        break;
-      case "contacts":
-        document.getElementById("contactsCardContent").style.display = "block";
-        break;
-      case "logIn":
-        document.getElementById("logInCardContent").style.display = "block";
-        break;
-      case "signUp":
-        document.getElementById("signUpCardContent").style.display = "block";
-        break;
-    }
+        if (document.getElementById("voteCardContent")) {
+            // using type input render the appropriate card innards
+            document.getElementById("voteCardContent").style.display = "block";
+        }
+    };
 
-<<<<<<< HEAD
     render() {
+        const { selectedIssue } = this.props;
+
         // <h4> should be in main card not innards
         return (
-            // this.props is coming from App.js --> Main.js --> Card.js
             <div id="issueCardContent" className="textBlock cardInnards">
-=======
-    // close sidebar after cards render
-    this.CloseSidebar();
-  };
->>>>>>> f6a2beebcf3a54bd9a75db3104d365b5a4a15064
+                {selectedIssue && (
+                    <div>
+                        <div className="headerContainer">
+                            <h3 classButton="bold">{selectedIssue.category}</h3>
+                        </div>
+                        <ul className="labelContainer">
+                            <li>
+                                <div>
+                                    {selectedIssue.images.map((imageURL) => {
+                                        return (
+                                            <div className="carasel">
 
-  GetDBIssues = () => {
-    console.log("event triggered!");
-  };
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            </li>
+                            <li className="labelContainer">
+                                <a>Status: {selectedIssue.status}</a>
+                                <a className="floatRight">Votes: {selectedIssue.voteCount}</a>
+                            </li>
 
-  render() {
-    const { selectedIssue } = this.props;
+                            <li className="labelContainer">
+                                {selectedIssue.descr}
+                            </li>
 
-    // <h4> should be in main card not innards
-    return (
-      <div id="issueCardContent" className="textBlock cardInnards">
-        {selectedIssue && (
-          <div>
-            <div className="headerContainer">
-              <h3 classButton="bold">{selectedIssue.category}</h3>
+                            {/* IYAN, THIS IS WHERE THE COMMENT SECTION SHOULD GO */}
+                            <li className="labelContainer">
+                            </li>
+
+
+
+                        </ul>
+                    </div>
+                )}
             </div>
-            <ul>
-              <li>{selectedIssue.descr}</li>
-            </ul>
-          </div>
-        )}
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default IssueCard;
