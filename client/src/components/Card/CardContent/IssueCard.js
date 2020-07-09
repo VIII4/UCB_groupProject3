3a6e82852581cfd282bf5aabd2cebb54eba135b6
Lay84 <Iyan.laylor@gmail.com>
2020-07-08 18:17:33 -0700

    comments handling and submission

--- client/src/components/Card/CardContent/IssueCard.js
+++ client/src/components/Card/CardContent/IssueCard.js
@@ -7,92 +7,132 @@ import "./IssueCard.css";
 import "../Card.css";
 
 class IssueCard extends React.Component {
-    // card methods
-    CloseCardInnards = () => {
-        // collect html collection --> convert to array
-        var cardInnardsList = Array.prototype.slice.call(
-            document.getElementsByClassName("cardInnards")
-        );
-
-        // hide all card innards with common class
-        cardInnardsList.forEach((element) => {
-            element.style.display = "none";
-        });
+  constructor(props) {
+    super(props);
+    this.state = {
+      commentValue: "",
     };
-    CloseCard = () => {
-        // hide card containter
-        document.getElementById("cardContainer").style.visibility = "hidden";
-        this.CloseCardInnards();
-    };
-    OpenCard = () => {
-        // first close any open cards
-        this.CloseCardInnards();
+  }
 
-        // show card parent
-        document.getElementById("cardContainer").style.visibility = "visible";
+  // card methods
+  CloseCardInnards = () => {
+    // collect html collection --> convert to array
+    var cardInnardsList = Array.prototype.slice.call(
+      document.getElementsByClassName("cardInnards")
+    );
 
-        if (document.getElementById("voteCardContent")) {
-            // using type input render the appropriate card innards
-            document.getElementById("voteCardContent").style.display = "block";
-        }
-    };
+    // hide all card innards with common class
+    cardInnardsList.forEach((element) => {
+      element.style.display = "none";
+    });
+  };
+  CloseCard = () => {
+    // hide card containter
+    document.getElementById("cardContainer").style.visibility = "hidden";
+    this.CloseCardInnards();
+  };
+  OpenCard = () => {
+    // first close any open cards
+    this.CloseCardInnards();
 
-    render() {
-        const { selectedIssue } = this.props;
+    // show card parent
+    document.getElementById("cardContainer").style.visibility = "visible";
 
-        // <h4> should be in main card not innards
-        return (
-            <div id="issueCardContent" className="textBlock cardInnards">
-                {selectedIssue && (
-                    <div>
-                        <div className="headerContainer">
-                            <h3 className="bold">{selectedIssue.category}</h3>
-                        </div>
-                        <ul className="labelContainer">
-                            <li>
-                                <div className="heightRestriction">
-                                    <Carousel>
-                                        {selectedIssue.images.map((imageURL) => {
-                                            return <img className="heightRestriction" src={imageURL} />;
-                                        })}
-                                    </Carousel>
-                                </div>
-                            </li>
-
-                            <li className="labelContainer">
-                                <a>Status: {selectedIssue.status}</a>
-                                <a className="floatRight">Votes: {selectedIssue.voteCount}</a>
-                            </li>
-
-                            <li className="labelContainer">{selectedIssue.descr}</li>
-
-                            <li>
-                                <p>Comments go here</p>
-                            </li>
-
-                            <li>
-                                <button onClick={this.props.onVoteClick}>Vote</button>
-                            </li>
-
-                            <li>
-                                <button onClick={this.props.onVoteClick}>Vote</button>
-                            </li>
-
-                            <li className="labelContainer">
-                                <textarea className="textBox" rows="4" cols="50" name="comment" form="usrform">
-                                    Enter text here...</textarea>
-                            </li>
-
-                            <li>
-                                <a onClick={this.OpenCard}>Back</a>
-                            </li>
-
-                        </ul>
-                    </div>
-                )}
-            </div>
-        );
+    if (document.getElementById("voteCardContent")) {
+      // using type input render the appropriate card innards
+      document.getElementById("voteCardContent").style.display = "block";
     }
+  };
+
+  handleInputChange = (event) => {
+    // Getting the value and name of the input which triggered the change
+    let value = event.target.value;
+    let name = event.target.name;
+
+    // Updating the input's state
+    this.setState({ [name]: value });
+  };
+
+  handleSubmitClick = (event) => {
+    alert("Hello");
+    event.preventDefault();
+    this.props.onCommentSubmission(
+      this.props.selectedIssue,
+      this.state.commentValue
+    );
+    this.setState({ commentValue: "" });
+  };
+
+  render() {
+    const { selectedIssue } = this.props;
+
+    // <h4> should be in main card not innards
+    return (
+      <div id="issueCardContent" className="textBlock cardInnards">
+        {selectedIssue && (
+          <div>
+            <div className="headerContainer">
+              <h3 className="bold">{selectedIssue.category}</h3>
+            </div>
+            <ul className="labelContainer">
+              <li>
+                <div className="heightRestriction">
+                  <Carousel>
+                    {selectedIssue.images.map((imageURL) => {
+                      return (
+                        <img className="heightRestriction" src={imageURL} />
+                      );
+                    })}
+                  </Carousel>
+                </div>
+              </li>
+
+              <li className="labelContainer">
+                <a>Status: {selectedIssue.status}</a>
+                <a className="floatRight">Votes: {selectedIssue.voteCount}</a>
+              </li>
+
+              <li className="labelContainer">{selectedIssue.descr}</li>
+
+              <li>
+                {selectedIssue.addtlcomments.map((commentObj) => {
+                  return <p>{commentObj.comment}</p>;
+                })}
+              </li>
+
+              <li>
+                <button onClick={this.props.onVoteClick}>Vote</button>
+              </li>
+
+              <li>
+                <button onClick={this.props.handleSubmitClick}>
+                  Add Comment
+                </button>
+              </li>
+
+              <li className="labelContainer">
+                <textarea
+                  value={this.state.commentValue}
+                  className="textBox"
+                  rows="4"
+                  cols="50"
+                  name="commentValue"
+                  onChange={this.handleInputChange}
+                  type="text"
+                  form="usrform"
+                  placeholder="Add comments here"
+                ></textarea>
+              </li>
+
+              <li>
+                <a onClick={this.OpenCard}>Back</a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default IssueCard;
